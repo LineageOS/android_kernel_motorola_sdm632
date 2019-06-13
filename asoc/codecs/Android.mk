c8d8e26cf3c9b53b50e179700516ef77267a2182
huangys <huangys@motorola.com>
2019-06-13 14:23:58 +0800

    ASoC: move cirrus playback driver here
    This driver code is copied from "motorola/kernel/module" bo branch.
    snapshot: ef5556a6694ff501dcf02ea31e704b3bb61ae651
    
    This driver is hard to share on all QC platform. But it's old location
    is  hoped to common for as most as product. Move it to QC audio kernel.
    It's seperated on different QC platform.
    
    (cherry-picked from (CR))
    
    Change-Id: I27b82772db94c3584d89992e1681a94e95a6f3e0
    Reviewed-on: https://gerrit.mot.com/1459200
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Submit-Approved: Jira Key

--- asoc/codecs/Android.mk
+++ asoc/codecs/Android.mk
@@ -144,6 +144,13 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
+include $(CLEAR_VARS)
+LOCAL_MODULE              := cirrus_playback.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+###########################################################
 
 endif # DLKM check
 endif # supported target check
