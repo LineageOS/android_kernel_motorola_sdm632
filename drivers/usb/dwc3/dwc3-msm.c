b71c8716de6b2f59616b12c52005728c3ce3eb5e
Jimmy Hu <hhhuuu@google.com>
2022-05-26 08:50:30 +0000

    usb: dwc3: Handle charging behavior when usb data is disabled
    Bug: 221009551
    Test: build, boot
    Signed-off-by: Jimmy Hu <hhhuuu@google.com>
    Change-Id: I82add2a6cb5e3060c930105b1b8b4f45da85f5f8

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3357,8 +3357,13 @@ static int dwc3_msm_vbus_notifier(struct notifier_block *nb,
 	/* Reset the Controller Error Count for Vbus change */
 	dwc->err_cnt = 0;
 
-	if (!mdwc->usb_data_enabled)
+	if (!mdwc->usb_data_enabled) {
+		if (event)
+			dwc3_msm_gadget_vbus_draw(mdwc, 500);
+		else
+			dwc3_msm_gadget_vbus_draw(mdwc, 0);
 		return NOTIFY_DONE;
+	}
 
 	dev_dbg(mdwc->dev, "vbus:%ld event received\n", event);
 
@@ -4822,7 +4827,8 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 			mdwc->drd_state = DRD_STATE_PERIPHERAL;
 			work = 1;
 		} else {
-			dwc3_msm_gadget_vbus_draw(mdwc, 0);
+			if (mdwc->usb_data_enabled)
+				dwc3_msm_gadget_vbus_draw(mdwc, 0);
 			dev_dbg(mdwc->dev, "Cable disconnected\n");
 		}
 		break;
