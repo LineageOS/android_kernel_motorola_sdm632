48dfcad53b9e23d7a14ecfa8954eab927a906f02
Georg Veichtlbauer <georg@vware.at>
2023-05-05 22:54:12 +0200

    Revert "fs/exec: fix use after free in execve"
    This reverts commit 00743ffde75b722ab619b7bc5b339df2f164f645.
    
    Change-Id: I61f9389fcd9ac3f09944c3a1ce7fff28eceb363f

--- fs/exec.c
+++ fs/exec.c
@@ -1698,7 +1698,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-	bool is_su;
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1819,7 +1818,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-	if (is_su && capable(CAP_SYS_ADMIN)) {
+	if (d_is_su(file->f_path.dentry) && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
 	}
